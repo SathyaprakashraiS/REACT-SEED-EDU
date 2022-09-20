f0c37e7ee7450aaee822118bca99c1a9950c0475
SATHYA <sathyaprakashrai2002@gmail.com>
2022-09-21 03:15:12 +0530

    contacts page updated with dynamic records

--- src/components/pages/contacts.js
+++ src/components/pages/contacts.js
@@ -1,9 +1,6 @@
 import React,{useState,useEffect, Component } from 'react';
 import Navbar from '../navbar';
 import './css/contacts.css';
-import { GoogleLogout } from 'react-google-login';
-import SNavbar from './snavbar';
-import BookStruct from '../structures/BookStruct';
 import { Link, Redirect, useHistory } from 'react-router-dom';
 
 function Contacts(){
@@ -26,8 +23,33 @@ function Contacts(){
       }
       }
     
-    useEffect(() => {
+      const [contacts,setcontacts] = useState([]);
+      async function fetchcontacts(stand) {
+        var apiavail=false;
+        var hlplnlink=`http://127.0.0.1:8000/helplinecontacts/`;
+        const request = await fetch(hlplnlink)
+          .then(response => {
+            if(response.ok)
+          {
+            console.log("here")
+            apiavail=true;
+            return response.json(); 
+          }
+          else{
+            console.log("im not here")
+          }
+        })
+          .then(data => { 
+            setcontacts(data)
+            console.log(setcontacts)
+          })
+          .catch((error) => {
+            console.log("the error ",error)
+          });
+        }
 
+    useEffect(() => {
+        fetchcontacts();
     }, []);
 
       console.log(userdata)
@@ -41,8 +63,28 @@ return(
     <Navbar/>
     <div className="inmain">
     <h1 style={center}><b>|_o_|</b></h1>
-    
-    
+    <b>CONTACTS</b>
+    {contacts.length>0 ? 
+    <div className="contacts">
+    {
+    contacts.map(item => (
+    <a key={item.id}>
+      <div className="contactcard">
+        <img className="bimg" src={item.img} /><br/>
+        <b>NAME: {item.contactname}</b><br/>
+        <b>CONTACT NUMBER: {item.contactnumber}</b><br/>
+        {
+            item.mail=="" || item.mail=="-"?<></>:<><b>E-MAIL: {item.mail}</b><br/></>
+        }
+        <p>{item.brief}</p>
+      </div>
+    </a>
+    ))
+    }
+    </div>
+    :
+    <p>NO contacts available :(</p>
+    }
     </div>
     
     </div>
